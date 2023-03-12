48340c572f89f40cb5b6b1c1a96bf4dd29b19502
koibhinheen <koibhinheen@gmail.com>
2023-03-12 18:45:46 +0500

    Largest Consecutive Sequence: Done

--- longest-consecutive-sequence/src/lib.rs
+++ longest-consecutive-sequence/src/lib.rs
@@ -1,3 +1,36 @@
+use std::collections::HashSet;
+
 pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-    todo!();
+    if nums.is_empty() {
+        return 0;
+    }
+
+    let mut nums_set = HashSet::new();
+    for n in nums.iter() {
+        nums_set.insert(*n);
+    }
+
+    let mut longest_length = 1;
+    for n in nums.iter() {
+
+        if nums_set.contains(&(n + 1)) {
+            continue;
+        }
+
+        let mut current_length = 1;
+        let mut next_in_sequence = n - 1;
+        loop {
+            if nums_set.contains(&next_in_sequence) {
+                current_length += 1;
+                if current_length > longest_length {
+                    longest_length = current_length;
+                }
+                next_in_sequence -= 1;
+            } else {
+                break;
+            }
+        }
+    }
+
+    return longest_length;
 }
